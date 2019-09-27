82d7edf2d4cdf43868b160178ea6b39ffde141a6
CBlockSurprise <CBlockSurprise@users.noreply.github.com>
2019-09-27 11:21:56 -0700

    Update script.js

--- projects/lastproject/script.js
+++ projects/lastproject/script.js
@@ -5,33 +5,41 @@ var zeros = {
   b: new ComplexNumber(-0.5,-(Math.sqrt(3)/2))
 }
 
+var zoom = 150;
+var resolution = 500;
+
+var offset = {
+  x: 0,
+  y: 0
+}
+
 function setup() {
   createCanvas(500,500);
   colorMode(HSB, 100);
   noStroke()
-  drawthething();
+  drawthething(); 
 }
 
 function drawthething() {
-
-  for (let i = -250; i < 250; i++) {
-    for (let j = -250; j < 250; j++) {
+  
+  for (let i = -250+offset.x; i < 250+offset.x; i++) {
+    for (let j = -250+offset.y; j < 250+offset.y; j++) {
       let f = new Polynomal([1,0,0,-1]);
       let fp = new Polynomal(f.difr());
       let nxii = 0;
-      let xn = new ComplexNumber(i/150,j/150);
+      let xn = new ComplexNumber(i/zoom,j/zoom);
       do {
         let y = f.evalulate(xn);
         let yp = fp.evalulate(xn);
         y.divide(yp);
         xn.subtraction(y);
         nxii++;
-      } while (isGreaterThanNums(xn) == null && nxii < 100);
+      } while (isGreaterThanNums(xn) == null && nxii < resolution);
       let color = isGreaterThanNums(xn) || 0;
-
+      
       fill(color,100,100);
       rect((i+250),(j+250),1,1);
-
+      
     }
   }
 }
@@ -41,7 +49,7 @@ var res = 0.5;
 function isGreaterThanNums(c) {
 
   if (c.dist(zeros.r) <= res) {
-
+    
     return 0;
   }
   if (c.dist(zeros.g) <= res) {
@@ -55,5 +63,5 @@ function isGreaterThanNums(c) {
 
 
 function draw() {
-
+  
 }
