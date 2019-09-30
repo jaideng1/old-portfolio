3927b715066670f19aca6b87311ba66b6af97b2e
CBlockSurprise <CBlockSurprise@users.noreply.github.com>
2019-09-30 13:19:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const inDevelopment = true;
+
+var strHTML = "";
+if (inDevelopment) {
+    strHTML = document.querySelector('body').innerHTML;
+    document.querySelector('body').innerHTML = "<h2>Sorry!</h2><br/><h3>The website is still in development.<br/><a href=\"https://github.com/CBlockSurprise\">To GitHub Page</a></h3>";
+}
