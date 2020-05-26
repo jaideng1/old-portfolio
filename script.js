cf32b56a568fcf10a864378f55d9ab384fe9dfb3
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-05-26 16:46:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const inDevelopment = true;
 var strHTML = "";
 if (inDevelopment) {
     strHTML = document.querySelector('body').innerHTML;
-    document.querySelector('body').innerHTML = "<h2>Sorry!</h2><br/><h3>The website is still in development.<br/><a href=\"https://github.com/CBlockSurprise\">To GitHub Page</a></h3>";
+    document.querySelector('body').innerHTML = "<h2>Sorry!</h2><br/><h3>The website is still in development.<br/><a href=\"https://github.com/jaideng1\">To GitHub Page</a></h3>";
 }
