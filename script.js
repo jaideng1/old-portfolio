2a652e661910e50858adc9d53ab90a7546cf037b
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-05-29 21:18:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const inDevelopment = true;
+const inDevelopment = false;
 var mainContent = "";
 var frame = 0;
 var inIntro = true;
@@ -10,7 +10,7 @@ function setup() {
     if (inDevelopment) {
         strHTML = document.querySelector('#main-content').innerHTML;
         document.querySelector('#main-content').innerHTML = "<h2>Sorry!</h2><br/><h3>The website is still in development.<br/><a href=\"https://github.com/jaideng1\">To GitHub Page</a></h3>";
-        document.querySelector('#main-content').style = "";
+        document.querySelector('#main-content').style = "display: block;";
         return;
     }
     strHTML = document.querySelector('#main-content').innerHTML;
