f2601ef52534ccb80da0fd66c664bf9ecc18faa3
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-05-29 21:16:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const inDevelopment = true;
 var mainContent = "";
+var frame = 0;
+var inIntro = true;
 
 //setup
 function setup() {
@@ -8,6 +10,7 @@ function setup() {
     if (inDevelopment) {
         strHTML = document.querySelector('#main-content').innerHTML;
         document.querySelector('#main-content').innerHTML = "<h2>Sorry!</h2><br/><h3>The website is still in development.<br/><a href=\"https://github.com/jaideng1\">To GitHub Page</a></h3>";
+        document.querySelector('#main-content').style = "";
         return;
     }
     strHTML = document.querySelector('#main-content').innerHTML;
@@ -15,6 +18,21 @@ function setup() {
     createCanvas(document.clientWidth, document.clientHeight);
 }
 
-function draw() {
+var WIDTH = document.clientWidth;
+var HEIGHT = document.clientHeight;
+
+let introStorage = {
     
+};
+
+function draw() {
+    if (inIntro) {
+        frame++;
+        let textbar = "";
+        if (frame % 180 < 90) {
+            textbar = "▮";
+        }
+        textSize(document.clientHeight / 30)
+        text("> " + textbar, document.clientWidth / 20, document.clientHeight / 20);
+    }
 }
