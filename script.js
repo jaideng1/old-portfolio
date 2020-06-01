2044512136e3a6b5175ff8856dba5d8d830ed4b6
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-06-01 16:11:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,10 @@ function draw() {
         text("> " + textbar, 50, 50);
     }
 }
+
+document.onkeypress = function(evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+    alert(charStr);
+};
