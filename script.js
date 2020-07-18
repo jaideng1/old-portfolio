0db66ea463212549a27002f0e013f286b9bd5132
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-07-17 19:49:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 
+var links = {
+  
+};
+
+function init() {
+  loadProjects();
+  fadeInLinks();
+}
+
+function fadeInLinks() {
+  setTimeout(function() {
+    let linkElements = document.querySelector("#social-links").children;
+    
+    for (let j = 0; j < linkElements[j]; j++) {
+      for (let i = 0; i < 100; i++) {
+        setTimeout(() => {
+          linkElements[j].style = "opacity: " + ((i + 1) * 100) + ";";
+        }, ((i + 1) * 10) + (j * 250));
+      }
+    }
+    
+  }, 750);
+}
+
+function loadProjects() {
+  
+}
+
+
+init();
+
+
+/*
+ @methods
+*/
+function clone(Obj) {
+  let buf; // the cloned object
+  if (Obj instanceof Array) {
+    buf = []; // create an empty array
+    var i = Obj.length;
+    while (i --) {
+      buf[i] = clone(Obj[i]); // recursively clone the elements
+    }
+    return buf;
+  } else if (Obj instanceof Object) {
+    buf = {}; // create an empty object
+    for (const k in Obj) {
+      if (obj.hasOwnProperty(k)) { // filter out another array's index
+        buf[k] = clone(Obj[k]); // recursively clone the value
+      }     
+    }
+    return buf;
+  } else {
+    return Obj;
+  }
+}
