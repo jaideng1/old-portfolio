df48b113acd62b810bf8a276fb635f1af2adb7ab
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-10-28 09:43:49 -0700

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -4,9 +4,7 @@ var searchbar = document.getElementById("searchbar");
 searchbar.addEventListener('input', openSearchBar);
 
 function openSearchBar(e) {
-  console.log("Activated");
   if (searchbar.value.length > 0) {
-    console.log("Activated 2");
-    document.href = "/search";
+    document.location.href = "/search";
   }
 }
