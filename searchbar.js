86ed78ea844d36752ed96bb33f384a2428b92d2a
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-10-28 13:20:37 -0700

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -3,8 +3,21 @@ var searchbar = document.getElementById("searchbar");
 
 searchbar.addEventListener('input', openSearchBar);
 
+var hasBeenBigger = false;
+
 function openSearchBar(e) {
   if (searchbar.value.length > 0) {
-    document.location.href = "/search.html";
+    if (searchbar.classList.contains("starting-searchbar")) {
+      searchbar.classList.remove("starting-searchbar");
+    }
+    if (searchbar.classList.contains("searchbar-small")) {
+      searchbar.classList.remove("searchbar-small");
+    }
+    searchbar.classList.add("searchbar-bigger");
+    hasBeenBigger = true;
+  }
+  if (searchbar.value.length == 0 && hasBeenBigger) {
+    searchbar.classList.remove("searchbar-bigger");
+    searchbar.classList.add("searchbar-small");
   }
 }
