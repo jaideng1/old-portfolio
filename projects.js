2dce2d4273d3f068dbaa65d27df124fb1168cbf6
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-12-04 13:19:56 -0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,77 +1,93 @@
+var tags = ["back-end","web design","math","design","game","school"]
+
 var projects = [
   //JS PROJECTS
   {
     title: "My Portfolio",
     link: "https://github.com/jaideng1/jaideng1.github.io/",
-    description: "I mean, it's one of my projects. You can see the source of it by clicking on \"My Portfolio,\" right above this."
+    description: "I mean, it's one of my projects. You can see the source of it by clicking on \"My Portfolio,\" right above this.",
+    tags: ["web design"]
   },
   {
     title: "Circles Are Cool",
     link: "https://github.com/jaideng1/circles_are_cool",
-    description: "Messing around with the properties of a circle, wanted to see what I could do with adjustable settings."
+    description: "Messing around with the properties of a circle, wanted to see what I could do with adjustable settings.",
+    tags: ["math","design"]
   },
   {
     title: "Newton's Fractal",
     link: "https://github.com/jaideng1/newtonsfractal",
-    description: "With a bit of help, I recreated Newton's Fractal. There is some stuff that could of been improved (as in shading, ect), but I wasn't able to understand that part of the problem."
+    description: "With a bit of help, I recreated Newton's Fractal. There is some stuff that could of been improved (as in shading, ect), but I wasn't able to understand that part of the problem.",
+    tags: ["math","desgin"]
   },
   {
     title: "Cretaceous Period Project",
     link: "https://github.com/jaideng1/cretaceous-period-project",
-    description: "A school project about the Cretaceous Period, a first attempt of trying to make a nice looking website. My porfolio is my second attempt, and it looks a lot better due to the things I learned from this project."
+    description: "A school project about the Cretaceous Period, a first attempt of trying to make a nice looking website. My porfolio is my second attempt, and it looks a lot better due to the things I learned from this project.",
+    tags: ["web design","school"]
   },
   {
     title: "Math Cardgame",
     link: "https://github.com/jaideng1/math-cardgame",
-    description: "A card game that I made for school (that's meant to be played online). The rules are a bit complicated though."
+    description: "A card game that I made for school (that's meant to be played online). The rules are a bit complicated though.",
+    tags: ["math","back-end","game"]
   },
   {
     title: "3D Circles with Spheres",
     link: "https://github.com/jaideng1/3d-circles",
-    description: "Testing out 3D objects with P5.js by using Spherical Coordinates."
+    description: "Testing out 3D objects with P5.js by using Spherical Coordinates.",
+    tags: ["math","design"]
   },
   {
     title: "Google Script Games",
     link: "https://github.com/jaideng1/google-script-games",
-    description: "Some games I made with Google Script."
+    description: "Some games I made with Google Script.",
+    tags: ["game","back-end"]
   },
   {
     title: "Google Script Spammer",
     link: "https://github.com/jaideng1/google-script-spammer",
-    description: "An email spammer I made with Google Script."
+    description: "An email spammer I made with Google Script.",
+    tags: ["back-end"]
   },
   //PYTHON
   {
     title: "Eyetracker",
     link: "https://github.com/jaideng1/python-eyetracker",
-    description: "An eyetracker that runs on python. I originally wanted this to connected to Minecraft, but you have to have a good camera to run it, which I don't have."
+    description: "An eyetracker that runs on python. I originally wanted this to connected to Minecraft, but you have to have a good camera to run it, which I don't have.",
+    tags: ["design","math"]
   },
   {
     title: "TicTacToe",
     link: "https://github.com/jaideng1/TicTacToe",
-    description: "A project I made 2 years ago when I started getting into Python. Due to me being new to this language, I made 300 lines of checks to see if someone had won, and another 300 for the AI checks."
+    description: "A project I made 2 years ago when I started getting into Python. Due to me being new to this language, I made 300 lines of checks to see if someone had won, and another 300 for the AI checks.",
+    tags: ["game"]
   },
   //JAVA
   {
     title: "Minecraft Capture the Flag",
     link: "https://github.com/jaideng1/minecraftctf",
-    description: "A basic project for Minecraft. It's a WIP, but I would like to start over and make it better in the future."
+    description: "A basic project for Minecraft. It's a WIP, but I would like to start over and make it better in the future.",
+    tags: ["game","design","back-end"]
   },
   {
     title: "Recreation of a Game",
     link: "https://jaideng1.github.io/java-projects/game-recreation.html",
-    description: "I don't exactly have a title for this, but it's a work in progress. It's a recreation of a game in Minecraft that I play a lot. The code isn't public right now though."
+    description: "I don't exactly have a title for this, but it's a work in progress. It's a recreation of a game in Minecraft that I play a lot. The code isn't public right now though.",
+    tags: ["game","back-end","design"]
   },
   //C#
   {
     title: "Data Crawler",
     link: "https://github.com/jaideng1/DataCrawler",
-    description: "A project I made to crawl around the files in MacOS and pick out files with certain keywords in the title. It takes a really long time to run though."
+    description: "A project I made to crawl around the files in MacOS and pick out files with certain keywords in the title. It takes a really long time to run though.",
+    tags: ["back-end"]
   },
   {
     title: "C# Chatter",
     link: "https://github.com/jaideng1/chatwithc",
-    description: "This is a program that connected with a webserver, and then you were able to chat with other people."
+    description: "This is a program that connected with a webserver, and then you were able to chat with other people.",
+    tags: ["back-end","design"]
   },
   
 ];
