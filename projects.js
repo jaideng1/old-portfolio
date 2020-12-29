be1e679114413b3321d4eb96883a46ba0e625923
jaideng1 <25446053+jaideng1@users.noreply.github.com>
2020-12-28 19:39:14 -0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -6,88 +6,102 @@ var projects = [
     title: "My Portfolio",
     link: "https://github.com/jaideng1/jaideng1.github.io/",
     description: "I mean, it's one of my projects. You can see the source of it by clicking on \"My Portfolio,\" right above this.",
-    tags: ["web design"]
+    tags: ["web design"],
+    language: "js",
   },
   {
     title: "Circles Are Cool",
     link: "https://github.com/jaideng1/circles_are_cool",
     description: "Messing around with the properties of a circle, wanted to see what I could do with adjustable settings.",
-    tags: ["math","design"]
+    tags: ["math","design"],
+    language: "js",
   },
   {
     title: "Newton's Fractal",
     link: "https://github.com/jaideng1/newtonsfractal",
     description: "With a bit of help, I recreated Newton's Fractal. There is some stuff that could of been improved (as in shading, ect), but I wasn't able to understand that part of the problem.",
-    tags: ["math","desgin"]
+    tags: ["math","desgin"],
+    language: "js",
   },
   {
     title: "Cretaceous Period Project",
     link: "https://github.com/jaideng1/cretaceous-period-project",
     description: "A school project about the Cretaceous Period, a first attempt of trying to make a nice looking website. My porfolio is my second attempt, and it looks a lot better due to the things I learned from this project.",
-    tags: ["web design","school"]
+    tags: ["web design","school"],
+    language: "js",
   },
   {
     title: "Math Cardgame",
     link: "https://github.com/jaideng1/math-cardgame",
     description: "A card game that I made for school (that's meant to be played online). The rules are a bit complicated though.",
-    tags: ["math","back-end","game"]
+    tags: ["math","back-end","game"],
+    language: "js",
   },
   {
     title: "3D Circles with Spheres",
     link: "https://github.com/jaideng1/3d-circles",
     description: "Testing out 3D objects with P5.js by using Spherical Coordinates.",
-    tags: ["math","design"]
+    tags: ["math","design"],
+    language: "js",
   },
   {
     title: "Google Script Games",
     link: "https://github.com/jaideng1/google-script-games",
     description: "Some games I made with Google Script.",
-    tags: ["game","back-end"]
+    tags: ["game","back-end"],
+    language: "js",
   },
   {
     title: "Google Script Spammer",
     link: "https://github.com/jaideng1/google-script-spammer",
     description: "An email spammer I made with Google Script.",
-    tags: ["back-end"]
+    tags: ["back-end"],
+    language: "js",
   },
   //PYTHON
   {
     title: "Eyetracker",
     link: "https://github.com/jaideng1/python-eyetracker",
     description: "An eyetracker that runs on python. I originally wanted this to connected to Minecraft, but you have to have a good camera to run it, which I don't have.",
-    tags: ["design","math"]
+    tags: ["design","math"],
+    language: "python",
   },
   {
     title: "TicTacToe",
     link: "https://github.com/jaideng1/TicTacToe",
     description: "A project I made 2 years ago when I started getting into Python. Due to me being new to this language, I made 300 lines of checks to see if someone had won, and another 300 for the AI checks.",
-    tags: ["game"]
+    tags: ["game"],
+    language: "python",
   },
   //JAVA
   {
     title: "Minecraft Capture the Flag",
     link: "https://github.com/jaideng1/minecraftctf",
     description: "A basic project for Minecraft. It's a WIP, but I would like to start over and make it better in the future.",
-    tags: ["game","design","back-end"]
+    tags: ["game","design","back-end"],
+    language: "java",
   },
   {
     title: "Recreation of a Game",
     link: "https://jaideng1.github.io/java-projects/game-recreation.html",
     description: "I don't exactly have a title for this, but it's a work in progress. It's a recreation of a game in Minecraft that I play a lot. The code isn't public right now though.",
-    tags: ["game","back-end","design"]
+    tags: ["game","back-end","design"],
+    language: "java",
   },
   //C#
   {
     title: "Data Crawler",
     link: "https://github.com/jaideng1/DataCrawler",
     description: "A project I made to crawl around the files in MacOS and pick out files with certain keywords in the title. It takes a really long time to run though.",
-    tags: ["back-end"]
+    tags: ["back-end"],
+    language: "c#",
   },
   {
     title: "C# Chatter",
     link: "https://github.com/jaideng1/chatwithc",
     description: "This is a program that connected with a webserver, and then you were able to chat with other people.",
-    tags: ["back-end","design"]
+    tags: ["back-end","design"],
+    language: "c#",
   },
   
 ];
